eb9020da572f5b4936e5cc5049cb66f8bbe3a154
Christian Williams <christianw@google.com>
2016-06-21 15:02:26 -0700

    DRY up `Shadows` generation.

---
+++ buildSrc/src/main/groovy/shadows.groovy
@@ -0,0 +1,44 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.tasks.compile.JavaCompile
+
+class ShadowsPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+        project.extensions.create("shadows", ShadowsPluginExtension)
+
+        project.configurations {
+            robolectricProcessor
+        }
+
+        project.dependencies {
+            robolectricProcessor project.project(":robolectric-processor")
+        }
+
+        project.sourceSets.main.java.srcDirs += project.files("${project.buildDir}/generated-shadows")
+
+        project.task("generateShadowProvider", type: JavaCompile) { task ->
+            classpath = project.configurations.compile + project.configurations.robolectricProcessor
+            source = project.sourceSets.main.java
+            destinationDir = project.file("${project.buildDir}/generated-shadows")
+
+            doFirst {
+                options.compilerArgs.addAll(
+                        "-proc:only",
+                        "-processor", "org.robolectric.annotation.processing.RobolectricProcessor",
+                        "-Aorg.robolectric.annotation.processing.shadowPackage=${project.shadows.packageName}"
+                )
+
+                println("doFirst! ${task.options.compilerArgs}")
+            }
+        }
+
+
+        def compileJavaTask = project.tasks["compileJava"]
+        compileJavaTask.dependsOn("generateShadowProvider")
+    }
+}
+
+class ShadowsPluginExtension {
+    String packageName
+}
